2786f92c0d93e56265481ade97ffee710a4c316f
liulingling <liulingling@astroway.net>
2018-03-12 16:16:14 +0800

    全职简历详情页面

---
+++ page/full-resume-detail/index.js
@@ -0,0 +1,55 @@
+/************兼职简历详情页面****************/
+var P = require('../../lib/wxpage')
+P('index', {
+    data: {
+    },
+
+    onLaunch: function () {
+    },
+
+    /**
+     * 初次加载页面配置
+     * **/
+    onLoad: function (options) {
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#37383b',
+        animation: {
+          duration: 400,
+          timingFunc: 'easeIn'
+        }
+      })
+      wx.setNavigationBarTitle({
+        title: '兼职岗位详情'
+      })
+      wx.showShareMenu({
+        withShareTicket: true
+      })
+    },
+
+    /**
+       * 下拉刷新
+       * **/
+    onPullDownRefresh: function () {
+      //this.getPayCourseList();
+      wx.stopPullDownRefresh()
+    },
+
+    /**
+     * 点击跳转到举报页面
+     * **/
+    toTipOffTap: function (e) {
+      wx.navigateTo({
+        url: "/page/tip-off-add/index?postId=1"
+      })
+    },
+
+    /**
+     * 点击跳转到企业详情
+     * **/
+    goCompanyDetail: function (e) {
+      wx.navigateTo({
+        url: "/page/company-detail/index?postId=1"
+      })
+    },
+})
