4e38bf4d14c1efe55caa120fb5abaad97f21569b
liulingling <liulingling@astroway.net>
2018-03-14 21:42:58 +0800

    企业中心（子账号列表，新增子账号，子账号设置，多地址管理列表，新增多地址）等页面的实现

---
+++ page/address-add/index.js
@@ -0,0 +1,39 @@
+/************岗位首页页面****************/
+var P = require('../../lib/wxpage')
+P('index', {
+    data: {
+      //2:检索分类
+      activeTypeId:1,     //切换兼职or全职
+    },
+
+    onLaunch: function () {
+    },
+
+    /**
+     * 初次加载页面配置
+     * **/
+    onLoad: function (options) {
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#37383b',
+        animation: {
+          duration: 400,
+          timingFunc: 'easeIn'
+        }
+      })
+      wx.setNavigationBarTitle({
+        title: '新增地址'
+      })
+      wx.showShareMenu({
+        withShareTicket: true
+      })
+    },
+
+    /**
+       * 下拉刷新
+       * **/
+    onPullDownRefresh: function () {
+      //this.getPayCourseList();
+      wx.stopPullDownRefresh()
+    },
+})
