d9495babd1526fa2dd02cfb1904fb96c42d49228
liulingling <liulingling@astroway.net>
2018-03-15 11:31:01 +0800

    创建了特工列表&&特工详情页面

---
+++ page/special-agent-detail/index.js
@@ -0,0 +1,37 @@
+/************全职页面详情****************/
+var P = require('../../lib/wxpage')
+P('index', {
+    data: {
+    },
+
+    onLaunch: function () {
+    },
+
+    /**
+     * 初次加载页面配置
+     * **/
+    onLoad: function (options) {
+      wx.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#37383b',
+        animation: {
+          duration: 400,
+          timingFunc: 'easeIn'
+        }
+      })
+      wx.setNavigationBarTitle({
+        title: '特工详情'
+      })
+      wx.showShareMenu({
+        withShareTicket: true
+      })
+    },
+
+    /**
+       * 下拉刷新
+       * **/
+    onPullDownRefresh: function () {
+      //this.getPayCourseList();
+      wx.stopPullDownRefresh()
+    },
+})
