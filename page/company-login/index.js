7cc08de08bb82547cbaea67956acc4189a47d1e1
liulingling <liulingling@astroway.net>
2018-03-24 16:58:25 +0800

    企业

--- page/company-login/index.js
+++ page/company-login/index.js
@@ -205,4 +205,95 @@ P('index', {
         }
       })
     },
+    //发布兼职按钮
+    goPartJob: function () {
+      wx.navigateTo({
+        url: '/page/company-release-part-job/index',
+      })
+    },
+    //发布全职按钮 
+    goFullJob: function () {
+      wx.navigateTo({
+        url: '/page/company-release-full-job/index',
+      })
+    },
+    //跳转智能岗位、平台推荐岗位、面试邀请记录、已投递的岗位
+    goJobList: function (e) {
+      if (e.currentTarget.dataset.id == '1') {
+        wx.navigateTo({
+          url: '/page/company-job-list/index',
+        })
+      } else {
+        wx.navigateTo({
+          url: '/page/alr-pay-resume-list/index?jobType=' + e.currentTarget.dataset.id,
+        })
+      }
+    },
+    //我的举报页面
+    goReportView: function () {
+      wx.navigateTo({
+        url: '/page/my-report-list/index',
+      })
+    },
+    //切换城市列表
+    goChangeCity: function () {
+      wx.navigateTo({
+        url: '/page/change-city/index',
+      })
+    },
+    /**
+     * 跳转到企业认证 1：编辑 2：进行中 3：已认证
+     */
+    goCompanyAudit: function (e) {
+      if (e.currentTarget.dataset.id == '1') {
+        wx.navigateTo({
+          url: '/page/company-audit-edite/index',
+        })
+      } else if (e.currentTarget.dataset.id == '2') {
+        wx.navigateTo({
+          url: '/page/company-audit-statues/index',
+        })
+      } else {
+        wx.navigateTo({
+          url: '/page/company-audit-finished/index',
+        })
+      }
+
+    },
+
+    /**
+     * 跳转招聘联系人列表
+     */
+    toContactList: function () {
+      wx.navigateTo({
+        url: '/page/contacts-list/index',
+      })
+    },
+
+    /**
+     * 跳转升级会员
+     */
+    toBuyVip: function () {
+      wx.navigateTo({
+        url: '/page/buy-vip/index',
+      })
+    },
+
+    /**
+     * 跳转到子账号列表
+     */
+    toSubAccountList: function () {
+      wx.navigateTo({
+        url: '/page/sub-account-list/index',
+      })
+    },
+
+    /**
+     * 跳转到工作多地址
+     */
+    toAddressList: function () {
+      wx.navigateTo({
+        url: '/page/address-list/index',
+      })
+    }
 })
